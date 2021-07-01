87eebf55965cd434032263c6328bf6ac205d5a49
renatobertone <cassimiro.bertone@gmail.com>
2021-07-01 12:49:15 -0300

    Cirando Classe Pessoa

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -0,0 +1,13 @@
+package one.digitalinnovation.digionebank
+
+class Pessoa {
+    var nome: String = "Renato"
+    var cpf: String = "123.123.123.11"
+}
+
+fun main() {
+    val teste = Pessoa() // Variável teste para chamar a classe Pessoa.
+
+    println(teste.nome)
+    println(teste.cpf)
+}
